0c4822ce86aa4a4ad62c471e3c72aa8641a6659b
Chris Onyedinma <chrisokem@yahoo.co.uk>
2017-03-25 15:09:03 +0000

    edit, views and index

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
   
+  def index
+    @lessons = Lesson.all
+  end
+  
   def new
     
     @lesson = Lesson.new
+    
+  end
+  
+  def edit
+    
+    @lesson = Lesson.find(params[:id])
     
   end
  
@@ -16,11 +26,23 @@ class LessonsController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      flash[:notice] = "Lesson updated"
+      redirect_to lesson_path(@lesson)
+    else
+      render 'new'
+    end
+  end
+  
+  
     def show
       
       @lesson = Lesson.find(params[:id])
-      
     end
+    
   private 
    def lesson_params
      params.require(:lesson).permit(:title, :description)
