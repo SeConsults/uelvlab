dadc43535c078a425bcc959c5eb85ac3fda5db05
Chris Onyedinma <chrisokem@yahoo.co.uk>
2017-03-25 16:37:53 +0000

    updated partial and delete action

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -43,6 +43,13 @@ class LessonsController < ApplicationController
       @lesson = Lesson.find(params[:id])
     end
     
+    def destroy
+      @lesson = Lesson.find(params[:id])
+      @lesson.destroy
+      flash[:notice] = "Lesson deleted"
+      redirect_to lessons_path
+    end
+    
   private 
    def lesson_params
      params.require(:lesson).permit(:title, :description)
